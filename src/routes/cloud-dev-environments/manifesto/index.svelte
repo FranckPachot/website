8b2e394214813cd4ec90ff138b6f949622881ee8
Julian <julian@gitpod.io>
2022-10-28 13:20:04 +0000

    feat: add explore card

--- src/routes/cloud-dev-environments/manifesto/index.svelte
+++ src/routes/cloud-dev-environments/manifesto/index.svelte
@@ -6,8 +6,11 @@
   import List from "$lib/components/cloud-dev-environments/manifesto/list.svelte";
   import Description from "$lib/components/cloud-dev-environments/manifesto/description.svelte";
   import Benefits from "$lib/components/cloud-dev-environments/manifesto/benefits.svelte";
+  import Explore from "$lib/components/explore.svelte";
 </script>
 
 <Description />
 <List manifestoItems={ManifestoItems} />
 <Benefits benefits={ManifestoBenefits} />
+
+<Explore contents={{ title: "Try it out" }} />
