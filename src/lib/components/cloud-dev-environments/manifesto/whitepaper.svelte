f65de3a6309e782d07724e9e599eb999f2f65458
Julian <julian@gitpod.io>
2022-10-29 08:29:55 +0000

    chore: update component

--- src/lib/components/cloud-dev-environments/manifesto/whitepaper.svelte
+++ src/lib/components/cloud-dev-environments/manifesto/whitepaper.svelte
@@ -1,18 +1,18 @@
 <script lang="ts">
-  import Hero from "$lib/components/whitepaper/cloud-dev-environments/hero.svelte";
   import Download from "$lib/components/whitepaper/cloud-dev-environments/download.svelte";
   import Section from "$lib/components/section.svelte";
+  import WhitepaperInfo from "./whitepaper-info.svelte";
 </script>
 
-<Section class="flex flex-wrap">
+<Section class="flex items-center gap-small lg:gap-0 flex-wrap">
   <div class="w-full lg:w-1/2">
-    <Hero
+    <WhitepaperInfo
       description="Curious how to supercharge your engineers with continuous development in the cloud? In this whitepaper we’ll establish the concept of cloud development environments (CDEs)."
       title="Cloud development environments"
       label="Whitepaper"
     />
   </div>
   <div class="w-full lg:w-1/2">
-    <Download toType="cde-whitepaper" eventType="Whitepaper" />
+    <Download class="mx-auto" toType="cde-whitepaper" eventType="Whitepaper" />
   </div>
 </Section>
